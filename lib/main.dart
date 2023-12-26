d28eea83d59efed72fad36adb9c81fb7b6b855c2
samuckqadev <dev.matsutech@gmail.com>
2023-12-25 21:12:41 -0300

    Modifing widget to StateFul

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,54 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
-void main(List<String> args) => runApp(const QuestionApp());
+void main(List<String> args) => runApp(QuestionApp());
 
-class QuestionApp extends StatelessWidget {
-  const QuestionApp({super.key});
+class QuestionAppState extends State<QuestionApp> {
+  var questionSelected = 0;
 
-  //widget = view component;
+  void toRespond() {
+    setState(() => questionSelected++);
+    print('Answer responded!');
+  }
 
   @override
   Widget build(BuildContext context) {
     //flutter most return values ro objects;
+    final List<String> questions = [
+      'What your favorite color?',
+      'What is your favorite food?',
+      'What is your favorite movie?',
+      'What is your favorite book?',
+      'What is your favorite sport?',
+      'What is your favorite Animal',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Questions'),
         ),
-        body: const Text('Hello Flutter'),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionSelected)),
+            ElevatedButton(
+                onPressed: () => toRespond(), child: Text('Answer 1')),
+            ElevatedButton(
+                onPressed: () => toRespond(), child: Text('Answer 2')),
+            ElevatedButton(
+                onPressed: () => toRespond(), child: Text('Answer 3'))
+          ],
+        ),
       ),
     );
+  }
+}
+
+class QuestionApp extends StatefulWidget {
+  //widget = view component;
+  @override
+  QuestionAppState createState() {
+    return QuestionAppState();
   }
 }
