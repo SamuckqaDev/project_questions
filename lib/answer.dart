8c2b7d4920923cb3d7b3ffaab0612c8562b3dfdd
samuckqadev <dev.matsutech@gmail.com>
2023-12-27 12:30:00 -0300

    Setting ElevatedButton(Answer compnent) styles

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final String text;
+  final void Function() onClick;
 
-  const Answer({super.key, required this.text});
+  const Answer({super.key, required this.text, required this.onClick});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: null, child: Text(text));
+    return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue[800],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          onPressed: onClick,
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.black, // Cor do texto
+            ),
+          ),
+        ));
   }
 }
